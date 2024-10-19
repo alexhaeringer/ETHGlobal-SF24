05fca95dee5bd801e880424d1f32909eb2ab7a71
alex <ge78koq@mytum.de>
2024-10-19 16:32:59 -0700

    picture work

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,20 +2,27 @@ import React from "react";
 
 function MovieList () {
     const movies = [
-        { id: 1, title: 'Interstellar', year: 2010 },
-        { id: 2, title: 'Mean Girls', year: 2004 }
+        { id: 1, title: 'Dune 2', year: 2010, image: 'Dune2.jpeg' },
+        { id: 2, title: 'Ex Machina', year: 2004, image: 'Exmachina.jpg' }
     ]
 
     return (
-        <div style = {{ color: 'white', fontFamily: 'Pixel-regular'}}>
-        <h2>Movies</h2>
-        <ul>
-            {movies.map((movie) => (
-            <li key={movie.id}>
-                {movie.title} ({movie.year})
-            </li>
-            ))}
-        </ul>
+        <div style={{ color: 'white', fontFamily: 'Pixel-regular' }}>
+            <h2>Movies</h2>
+            <ul style={{ listStyleType: 'none', padding: 0 }}>
+                {movies.map((movie) => (
+                    <li key={movie.id} style={{ marginBottom: '20px' }}>
+                        <img
+                            src={`${process.env.PUBLIC_URL}/images/${movie.image}`}
+                            alt={movie.title}
+                            style={{ width: '150px', height: 'auto', marginRight: '10px' }}
+                        />
+                        <div>
+                            {movie.title} ({movie.year})
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
